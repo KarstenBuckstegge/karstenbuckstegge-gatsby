e262bd4b2210a7a7527b36f86544532bbbf75af4
Karsten Buckstegge <k.buckstegge@gmail.com>
2018-12-10 21:11:43 +0100

    connect gatsby to contentful

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,48 @@
 import React from "react"
+import { StaticQuery, graphql } from "gatsby";
 
-export default () => <div>Hello worldddd!</div>
+const Homepage = () => (
+    <StaticQuery
+        query={graphql`
+            query ContentBlock {
+                contentfulContentBlock {
+                    title
+                    subtitle
+                    description {
+                        content {
+                            content {
+                                value
+                                marks {
+                                    type
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        `}
+    >
+        {(contentBlock) => {
+            const {
+                contentfulContentBlock: {
+                    title,
+                    subtitle,
+                    description
+                }
+            } = contentBlock;
+
+            return (
+                <section>
+                    <h1>{title}</h1>
+                    <h2>{subtitle}</h2>
+                    <p>
+                        {JSON.stringify(description)}
+                    </p>
+                </section>
+            );
+        }}
+    </StaticQuery>
+);
+
+
+export default Homepage;
