e66e4eb910c327491e0667ac0208697bb95bbf60
Karsten Buckstegge <k.buckstegge@gmail.com>
2019-09-24 13:10:59 +0200

    feat: add basic menu markup

--- src/components/MenuComponent/MenuComponent.tsx
+++ src/components/MenuComponent/MenuComponent.tsx
@@ -1,3 +1,19 @@
 import * as React from 'react';
 
-export const MenuComponent: React.StatelessComponent = () => <div>MENU</div>;
+import classnames from 'classnames';
+
+import styles from './menuComponent.module.scss';
+
+export const MenuComponent: React.StatelessComponent = () => (
+  <ul className={styles.menu}>
+    <a href="#" className={styles.link}>
+      <li className={classnames(styles.item, styles.portfolio)}>portfolio</li>
+    </a>
+    <a href="#" className={styles.link}>
+      <li className={classnames(styles.item, styles.about)}>about me</li>
+    </a>
+    <a href="#" className={styles.link}>
+      <li className={classnames(styles.item, styles.contact)}>contact</li>
+    </a>
+  </ul>
+);
