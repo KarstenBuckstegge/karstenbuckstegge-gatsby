d562c5609a557ce684bc743bd262053d898feaa7
Karsten Buckstegge <k.buckstegge@gmail.com>
2019-09-27 15:05:32 +0200

    feat: add button to contact form

--- src/components/ContactComponent/ContactComponent.tsx
+++ src/components/ContactComponent/ContactComponent.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
 
+import { Button } from '../../patterns/Button/Button';
 import { Headline } from '../../patterns/Headline/Headline';
 import { Input } from '../../patterns/Input/Input';
 import { Textarea } from '../../patterns/Textarea/Textarea';
+
+import styles from './contactComponent.module.scss';
 
 interface Props {
   className?: string;
@@ -11,11 +14,14 @@ interface Props {
 export const ContactComponent: React.StatelessComponent<Props> = props => (
   <section className={props.className}>
     <Headline>talk to me...</Headline>
-    <form action="">
+    <form className={styles.form} action="">
       <Input type="text" name="name" id="name" label="Name" autoComplete="name" required={true} />
       <Input type="email" name="email" id="email" label="Email" autoComplete="email" required={true} />
       <Input type="text" name="topic" id="topic" label="Topic" required={true} />
       <Textarea name="message" id="message" label="Your Message" rows={2} required={true} />
+      <Button className={styles.button} color="green" size="small">
+        Send
+      </Button>
     </form>
   </section>
 );
