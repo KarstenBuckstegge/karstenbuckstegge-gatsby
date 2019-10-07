9695f9aa75851dc21a1a82ad389caf82f4847be3
Karsten Buckstegge <k.buckstegge@gmail.com>
2019-10-07 18:32:48 +0200

    possible netlify build fix
    by adding case sensitivity config to git
    https://github.com/gatsbyjs/gatsby/issues/8205\#issuecomment-444876531

---
+++ src/components/LayoutComponent/LayoutComponent.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+import { HeaderComponent } from '../HeaderComponent/HeaderComponent';
+
+import styles from './layoutComponent.module.scss';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const LayoutComponent: React.StatelessComponent<Props> = ({ children }) => {
+  // require('smooth-scroll')('a[href*="#"]', {
+  //   speed: 600
+  // });
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.grain} />
+      <HeaderComponent />
+      {children}
+    </main>
+  );
+};
