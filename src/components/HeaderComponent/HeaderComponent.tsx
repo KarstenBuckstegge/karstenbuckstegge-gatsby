77061a7c6d807084de7de4ebb45acb0efbe13ba1
Karsten Buckstegge <k.buckstegge@gmail.com>
2020-04-03 15:58:35 +0200

    feat: add social buttons to header

--- src/components/HeaderComponent/HeaderComponent.tsx
+++ src/components/HeaderComponent/HeaderComponent.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 import { transitionDirection } from '../../containers/PageTransitionContainer';
+import { Icon } from '../../patterns/Icon/Icon';
 import { LinkButton } from '../../patterns/LinkButton/LinkButton';
 import { LogoComponent } from '../LogoComponent/LogoComponent';
 import { MenuComponent } from '../MenuComponent/MenuComponent';
@@ -17,16 +18,35 @@ export const HeaderComponent: React.StatelessComponent<Props> = props => (
   <header className={styles.header}>
     <LogoComponent link="/" className={styles.logo} />
     {props.secondaryPage ? (
-      <LinkButton
-        internal={true}
-        href="/"
-        className={styles.backButton}
-        color="green"
-        size="small"
-        direction={props.pageTransitionDirection}
-      >
-        Back to MainPage
-      </LinkButton>
+      <div className={styles.right}>
+        <div className={styles.social}>
+          <a className={styles.socialButton} target="_blank" href="https://www.instagram.com/karstenbuckstegge/">
+            <Icon className={styles.socialIcon} icon="instagram" />
+          </a>
+          <a className={styles.socialButton} target="_blank" href="https://www.behance.net/karstenbuckstegge/">
+            <Icon className={styles.socialIcon} icon="behance" />
+          </a>
+          <a className={styles.socialButton} target="_blank" href="https://www.github.com/karstenbuckstegge/">
+            <Icon className={styles.socialIcon} icon="github" />
+          </a>
+          <a className={styles.socialButton} target="_blank" href="https://www.linkedin.com/in/karstenbuckstegge/">
+            <Icon className={styles.socialIcon} icon="linkedin" />
+          </a>
+          <a className={styles.socialButton} target="_blank" href="https://www.xing.com/profile/Karsten_Buckstegge/">
+            <Icon className={styles.socialIcon} icon="xing" />
+          </a>
+        </div>
+        <LinkButton
+          internal={true}
+          href="/"
+          className={styles.backButton}
+          color="green"
+          size="small"
+          direction={props.pageTransitionDirection}
+        >
+          Back to MainPage
+        </LinkButton>
+      </div>
     ) : (
       <MenuComponent headerOffScreen={props.headerOffScreen} />
     )}
